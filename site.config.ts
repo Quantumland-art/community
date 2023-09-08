9780f161df67a6bc2a64c44eb11d89af2f1c2081
omarcostahamido <18335360+omarcostahamido@users.noreply.github.com>
2023-09-08 02:50:41 +0100

    Update site.config.ts
    - add quantuland.art github
    - set custom navigation

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: 'omarcostahamido',
-  github: 'omarcostahamido',
+  github: 'Quantumland-art/community',
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   newsletter: 'https://quantumland.art?subscribe=1', // optional newsletter URL
@@ -50,16 +50,20 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    // {
+    //   title: 'About',
+    //   pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+    // },
+    // {
+    //   title: 'Contact',
+    //   pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+    // },
+    {
+      title: 'Forum',
+      pageId: 'https://github.com/orgs/Quantumland-art/discussions'
+    }
+  ]
 })
