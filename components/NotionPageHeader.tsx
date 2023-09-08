b8380caf3b8423a615cc96d49840c36278bd6204
omarcostahamido <18335360+omarcostahamido@users.noreply.github.com>
2023-09-08 02:53:24 +0100

    Update NotionPageHeader.tsx
    - complete breadcrumbs when not in default mode.

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -45,7 +45,7 @@ export const NotionPageHeader: React.FC<{
   return (
     <header className='notion-header'>
       <div className='notion-nav-header'>
-        <Breadcrumbs block={block} rootOnly={true} />
+        <Breadcrumbs block={block} rootOnly={false} />
 
         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks
