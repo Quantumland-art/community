77ad7650d2d6bf98ad51716b32be202ed9bced61
Omar Costa Hamido <18335360+omarcostahamido@users.noreply.github.com>
2023-10-27 15:29:44 +0100

    Update last_modified_at_generator.rb
    - rename the collection dependency

--- _plugins/last_modified_at_generator.rb
+++ _plugins/last_modified_at_generator.rb
@@ -8,7 +8,7 @@ module Recents
   # Generate change information for all markdown pages
   class Generator < Jekyll::Generator
     def generate(site)
-      items = site.collections['notes'].docs
+      items = site.collections['content'].docs
       items.each do |page|
         timestamp = Jekyll::LastModifiedAt::Determinator.new(site.source, page.path, '%FT%T%:z').to_s
         page.data['last_modified_at_timestamp'] = timestamp
